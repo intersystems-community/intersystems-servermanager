528555b25cecdf8aa1620965539175b0eb22c724
gjsjohnmurray <johnm@georgejames.com>
2023-07-24 14:43:00 +0100

    Remove keytar, legacy v2 password handling, and `intersystemsServerManager.authentication.provider` setting

--- src/api/getServerSpec.ts
+++ src/api/getServerSpec.ts
@@ -1,8 +1,5 @@
 import * as vscode from "vscode";
-import { AUTHENTICATION_PROVIDER } from "../authenticationProvider";
-import { filePassword } from "../commands/managePasswords";
 import { IServerSpec } from "@intersystems-community/intersystems-servermanager";
-import { Keychain } from "../keychain";
 
 interface ICredentialSet {
 	username: string;
@@ -36,128 +33,17 @@ export async function getServerSpec(
 		return undefined;
 	}
 
-	const useOurAuthProvider = (
-		vscode.workspace
-			.getConfiguration("intersystemsServerManager.authentication")
-			.get<string>("provider", "") === AUTHENTICATION_PROVIDER
-	);
-
 	server.name = name;
 	server.description = server.description || "";
 	server.webServer.scheme = server.webServer.scheme || "http";
 	server.webServer.port = server.webServer.port || (server.webServer.scheme === "https" ? 443 : 80);
 	server.webServer.pathPrefix = server.webServer.pathPrefix || "";
 
-	if (noCredentials && !useOurAuthProvider) {
-		server.username = undefined;
+
+	// When authentication provider is being used we should only have a password if it came from the deprecated
+	// property of the settings object. Otherwise return it as undefined.
+	if (!server.password) {
 		server.password = undefined;
-	} else {
-
-		// Use cached username if appropriate
-		if (!server.username && !useOurAuthProvider && credentialCache[name]) {
-			server.username = credentialCache[name].username;
-		}
-
-		// Prompt for a username if necessary (including blank to try connecting anonymously)
-		if (!server.username && !useOurAuthProvider) {
-			await vscode.window
-				.showInputBox({
-					ignoreFocusOut: true,
-					placeHolder: `Username to connect to InterSystems server '${name}' as`,
-					prompt: "Leave empty to attempt unauthenticated access",
-				})
-				.then((username) => {
-					if (username && server) {
-						server.username = username;
-					} else {
-						return undefined;
-					}
-				});
-			if (!server.username) {
-				server.username = "";
-				server.password = "";
-			}
-		}
-
-		// Obtain password from session cache or keychain
-		// unless trying to connect anonymously or using the AuthenticationProvider
-		if (server.username && !server.password && !useOurAuthProvider) {
-			if (credentialCache[name] && credentialCache[name].username === server.username) {
-				server.password = credentialCache[name].password;
-			} else {
-				const keychain = new Keychain(name);
-				const password = await keychain.getPassword().then((result) => {
-					if (typeof result === "string") {
-						return result;
-					} else {
-						return undefined;
-					}
-				});
-				if (password) {
-					server.password = password;
-					credentialCache[name] = { username: server.username, password };
-				}
-			}
-
-		}
-		if (server.username && !server.password && !useOurAuthProvider) {
-			const doInputBox = async (): Promise<string | undefined> => {
-				return await new Promise<string | undefined>((resolve, reject) => {
-					const inputBox = vscode.window.createInputBox();
-					inputBox.password = true,
-						inputBox.title = `Password for InterSystems server '${name}'`,
-						inputBox.placeholder = `Password for user '${server?.username}' on '${name}'`,
-						inputBox.prompt = "To store your password securely, submit it using the $(key) button",
-						inputBox.ignoreFocusOut = true,
-						inputBox.buttons = [
-							{ iconPath: new vscode.ThemeIcon("key"), tooltip: "Store Password in Keychain" },
-						];
-
-					async function done(store: boolean) {
-						// Optionally file the password, then return it
-						const password = inputBox.value;
-						if (store) {
-							await filePassword(name, password)
-						}
-						// Resolve the promise and tidy up
-						resolve(password);
-						inputBox.dispose();
-					}
-
-					inputBox.onDidTriggerButton((button) => {
-						// We only added one button, and user clicked it
-						done(true);
-					});
-
-					inputBox.onDidAccept(() => {
-						// User pressed Enter
-						done(false);
-					});
-
-					inputBox.onDidHide(() => {
-						// User pressed Escape
-						resolve(undefined);
-						inputBox.dispose();
-					});
-
-					inputBox.show();
-				});
-			};
-			await doInputBox().then((password) => {
-				if (password && server) {
-					server.password = password;
-					credentialCache[name] = { username: server.username, password };
-				} else {
-					server = undefined;
-				}
-			});
-		}
-
-		// When authentication provider is being used we should only have a password if it came from the deprecated
-		// property of the settings object. Otherwise return it as undefined.
-		if (useOurAuthProvider && !server.password) {
-			server.password = undefined;
-		}
 	}
 	return server;
 }
