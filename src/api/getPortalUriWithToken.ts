231815c58133f48ca58aa15e3ac45cd8c7136c0b
gjsjohnmurray <johnm@georgejames.com>
2026-02-09 09:02:43 +0000

    Remove remnants of Simple Browser use

--- src/api/getPortalUriWithToken.ts
+++ src/api/getPortalUriWithToken.ts
@@ -5,14 +5,11 @@ import { extensionId } from "../commonActivate";
 import { makeRESTRequest } from "../makeRESTRequest";
 
 export enum BrowserTarget {
-	SIMPLE = 0,
 	EXTERNAL = 1,
 	INTEGRATED = 2,
 }
 
 const allTokens = [new Map<string, string>(), new Map<string, string>(), new Map<string, string>()];
-
-const simpleBrowserCompatible = new Map<string, boolean>();
 
 export async function getPortalUriWithToken(
 	target: BrowserTarget,
@@ -28,43 +25,6 @@ export async function getPortalUriWithToken(
 	const spec: IServerSpec | undefined = await myApi.getServerSpec(name, scope);
 	if (typeof spec !== "undefined") {
 
-		if (target === BrowserTarget.SIMPLE && !simpleBrowserCompatible.has(name)) {
-			// Check that the portal webapps have all been altered so they don't require session cookie support, which Simple Browser cannot provide
-			const response = await makeRESTRequest(
-				"POST",
-				spec,
-				{ apiVersion: 1, namespace: "%SYS", path: "/action/query" },
-				{
-					query:
-						"SELECT Name FROM Security.Applications WHERE {fn CONCAT(Name, '/')} %STARTSWITH '/csp/sys/' AND UseCookies = 2 " +
-						"UNION SELECT $PIECE($PIECE($PIECE($ZVERSION,') ',2),' '),'.') AS Name"
-				},
-			).catch((errorStr) => {
-				vscode.window.showErrorMessage(`Error checking the Portal web apps for compatibility with Simple Browser${errorStr ? `: ${errorStr}` : "."}`, "Dismiss");
-			});
-			if (response) {
-				const appsRequiringCookie = (response.data?.result?.content as any[]).map((row) => {
-					return row.Name as string;
-				});
-				if (appsRequiringCookie.length && parseInt(appsRequiringCookie[appsRequiringCookie.length - 1], 10) >= 2024) {
-					// SMP in 2024.1+ can't be embedded in a cross-origin iframe
-					vscode.window.showWarningMessage(`The Portal cannot be opened in the Simple Browser for IRIS versions 2024.1+.`, "Dismiss");
-					simpleBrowserCompatible.set(name, false);
-				}
-				else if (appsRequiringCookie.length > 1) {
-					vscode.window.showWarningMessage(`Portal web apps cannot be used in the Simple Browser tab if their 'UseCookies' property is set to 'Always' (the default). To resolve this, use Portal's security section to change it to 'Autodetect' in these apps: ${appsRequiringCookie.slice(0, -1).join(", ")}`, { modal: true });
-				}
-				else {
-					simpleBrowserCompatible.set(name, true);
-				}
-			}
-			if (!simpleBrowserCompatible.get(name)) return;
-		}
-
-		if (target === BrowserTarget.SIMPLE && simpleBrowserCompatible.has(name) && !simpleBrowserCompatible.get(name)) {
-			vscode.window.showWarningMessage(`The Portal cannot be opened in the Simple Browser for IRIS versions 2024.1+.`, "Dismiss");
-			return;
-		}
 
 		// Retrieve previously cached token
 		let token = allTokens[target].get(name) || "";
